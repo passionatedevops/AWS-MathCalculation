c645aad67ddf9cc1150b38947a4652025069ec4b
passionatedevops <suman.sarkar@carsales.com.au>
2025-02-23 02:50:20 +1100

    update index.html

---
+++ Execution Role Policy.js
@@ -0,0 +1,13 @@
+{
+    "Sid": "VisualEditor0",
+    "Effect": "Allow",
+    "Action": [
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:Scan",
+        "dynamodb:Query",
+        "dynamodb:UpdateItem"
+    ],
+    "Resource": "arn:aws:dynamodb:ap-southeast-2:832922929464:table/AWS-MathCalculation-DB"
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
+# wrtite the read fiel file 
 Add readme fiel content. this is a small project , deployed to AWS resources using amplify , api gateway, lambda fucntion , deynamoDB)
+
+
+Tages all resources with 
+
+Owner : DevOps-project
+
+
+
+Reference document : https://www.youtube.com/watch?v=7m_q1ldzw0U&t=0s
